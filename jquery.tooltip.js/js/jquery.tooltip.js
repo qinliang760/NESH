622db1ee46c655c26d21b4af6a80c11064afef0f
qinliang <qinliang@163.com>
2012-12-20 22:35:30 +0800

    add share tooltip plugin

--- jquery.tooltip.js/js/jquery.tooltip.js
+++ jquery.tooltip.js/js/jquery.tooltip.js
@@ -4,65 +4,67 @@
  * Libs:Tooltip
  * Copyright 2011, Mediav
  */
- 
-(function($){
-	$.fn.tooltip=function(options){
-		//default options
-		var defaults={
-			tipWidth:140
-		}
-		var o=$.extend(defaults,options);
-		return this.each(function(){
-			var t=$(this);
-			t.hover(function(e){
-				
-				
-				//tooltip position and fx
-				$("#titleTip").css({"top":(e.pageY+10)+"px","left":(e.pageX+10)+"px"}).show("fast").stop(true,true);
-				
-				//width adapt window
-				
-			},function(){
-				this.title=this.showTitle;
-				$("#titleTip").remove();
-			})
-		})
-	}	  
 
-
-})(jQuery);
-
-(function($){
-	tooltip={
-		defaults:{
-			tipWidth:140
+(function($) {
+	tooltip = {
+		defaults: {
+			tipWidth: 140
 		}
 	}
-	
-	function Tooltip(root,defaults){
-		var self=this,
-			obj=root;
-		$.extend(this,{
-			autoWin:function(){
-			if($(window).width()<$(document).width()){	
-					$("#titleTip").css({"top":(e.pageY+10)+"px","left":(e.pageX-150)+"px"}).show("fast").stop(true,true);	
-				}
+
+	function Tooltip(root, defaults) {
+		var self = this,
+		obj = root;
+		$.extend(this, {
+			setTip: function(leftV, topV) {
+
+				$("#titleTip").css({
+					"top": topV + "px",
+					"left": leftV + "px"
+				}).show("fast").stop(true, true);
 			},
-			appendTip:function(){
-				this.showTitle=this.title;
-				this.title="";
-				var tooltip="<div id='titleTip' style='width:"+o.tipWidth+"px'>"+this.showTitle+"</div>";
-				$("body").append(tooltip);
+			appendTip: function() {
+
+				obj.hover(function(e) {
+					this.showTitle = this.title;
+					this.title = "";
+					var tooltip = "<div id='titleTip' style='width:" + defaults.tipWidth + "px'>" + this.showTitle + "</div>";
+					$("body").append(tooltip);
+					var ex = e.pageX,
+					ey = e.pageY,
+					tooltipW = $("#titleTip").width(),
+					tooltipH = $("#titleTip").height(),
+					wW = $(window).width(),
+					wH = $(window).height(),
+					leftV,
+					topV;
+					if (ex + tooltipW > wW) {
+						leftV = ex - (ex + tooltipW - wW);
+					} else {
+						leftV = ex;
+					}
+					if (ey + tooltipH > wH) {
+						topV = ey - (ey + tooltipH - wH);
+					} else {
+						topV = ey;
+					}
+					self.setTip(leftV, topV);
+				},
+				function() {
+					$("#titleTip").remove();
+				})
 			}
-			
+
 		})
 	}
 
-	$.fn.tooltip=function(defaults){
-		defaults=$.extend({},tooltip.defaults,defaults);
-		return this.each(function(){
-			new Tooltip($(this),defaults);
+	$.fn.tooltip = function(defaults) {
+		defaults = $.extend({},
+		tooltip.defaults, defaults);
+		return this.each(function() {
+			new Tooltip($(this), defaults);
 		})
 	}
 
 })(jQuery);
+
